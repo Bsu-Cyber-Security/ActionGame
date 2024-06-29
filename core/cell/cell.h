489965df6723d937de013d76c093d742bd969c52
NIKITA <nikitosina220808@gmail.com>
2024-06-30 00:58:20 +0300

    Обновлены файлы cell.h и player.h

--- core/cell/cell.h
+++ core/cell/cell.h
@@ -1,1 +1,79 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <QPixmap>
+
+class Cell
+{
+   private:
+    int m_x = 0;
+    int m_y = 0;
+    int m_width = 0;
+    int m_height = 0;
+    bool m_CanMove = true;
+    QPixmap m_image;
+   public:
+    Cell()= default;
+    Cell(int x, int y, int width, int height, bool CanMove,  QPixmap& image): m_x(x), m_y(y), m_width(width), m_height(height), m_CanMove(CanMove), m_image(image) {}
+
+    int x()
+    {
+        return m_x;
+    }
+    
+    int y()
+    {
+        return m_y;
+    }
+
+    int width()
+    {
+        return m_width;
+    }
+
+    int height()
+    {
+        return m_height;
+    }
+
+    bool CanMove()
+    {
+        return m_CanMove;
+    }
+
+    const QPixmap& image()
+    {
+        return m_image;
+    }
+
+    void setX(int x)
+    {
+        m_x = x;
+    }
+
+    void setY(int y)
+    {
+        m_y = y;
+    }
+
+    void setWIDTH(int width)
+    {
+        m_width = width;
+    }
+
+    void setHEIGHT(int heigth)
+    {
+        m_height = heigth;
+    }
+
+    void setCANMOVE(bool CanMove)
+    {
+        m_CanMove = CanMove;
+    }
+
+    void setImage(QPixmap& image)
+    {
+        m_image = image;
+    }
+
+};
--- core/player/player.h
+++ core/player/player.h
@@ -1,3 +1,42 @@
 #pragma once
+#include <string>
 
-struct Player;
+class Player
+{
+   private:
+    std::string m_name;
+    int m_x;
+    int m_y;
+
+   public:
+    std::string name()
+    {
+        return m_name;
+    }
+
+    int x()
+    {
+        return m_x;
+    }
+
+    int y()
+    {
+        return m_y;
+    }
+
+    void setNAME(std::string name)
+    {
+        m_name = name;
+    }
+
+    void setX(int x)
+    {
+        m_x = x;
+    }
+
+    void setY(int y)
+    {
+        m_y = y;
+    }
+
+};
