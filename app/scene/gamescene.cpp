aa13ba94ae39d33d82a6a9f85b3766af5a4c0168
NIKITA <nikitosina220808@gmail.com>
2024-06-30 19:49:53 +0300

    Обновлен файл gamescene.cpp добавлен файл map.txt

--- app/scene/gamescene.cpp
+++ app/scene/gamescene.cpp
@@ -12,16 +12,51 @@
 #include "app/scene/gamescene.h"
 #include "app/pixmap_manager/pixmap_manager.h"
 
+void GameScene::readLevelsFile(QString pathFile)
+{
+    std::vector<std::vector<char>> map(20, std::vector<char>(20));
+
+    QFile file(pathFile);
+
+    if (file.open(QIODevice::ReadOnly))
+    {
+        QTextStream in(&file);
+
+        for (int i = 0; i < 20; ++i)
+        {
+            QString line = in.readLine();
+            QStringList cells = line.split(' ');
+
+            if (cells.size() == 20)
+            {
+                for (int j = 0; j < 20; ++j)
+                {
+                    map[i][j] = cells[j].toLatin1()[0];
+                }
+            }
+
+            else
+            {
+                qDebug() << "Error: Invalid map data in line" << i + 1;
+            }
+        }
+
+        file.close();
+        }
+        else
+        {
+        qDebug() << "Error: Failed to open file" << pathFile;
+        }
+}
+
 GameScene::GameScene(QObject *parent)
     : QGraphicsScene(parent)
     , m_mapNeedRedraw(false)
     , m_levelIsCompleted(false)
     , m_cameraOffsetX(0)
     , m_cameraOffsetY(0) {
-    setSceneRect(0,0, 800, 800);
+    setSceneRect(0,0, 640, 480);
     //connect(&m_timer, &QTimer::timeout, this, &GameScene::loop);
     //m_timer.start(int(1000.0f/FPS));
-    //m_elapsedTimer.start();
-    readLevelsFile("");
-    runLevel();
+    m_elapsedTimer.start();
 }
